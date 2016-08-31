8088a7c44685d823f755386ba2e2707f8bc61c89
Tony Dimitrov <01tonydimitrov@gmail.com>
2016-08-31 22:52:33 +0300

    Testing git commit
    Testing

--- BudgetDB/Models/Budget.cs
+++ BudgetDB/Models/Budget.cs
@@ -1,14 +1,16 @@
 ﻿
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 
 
 namespace BudgetDB
 {
+
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     public class Budget// : IBudget
     {
         public Budget(decimal totalBudget)
